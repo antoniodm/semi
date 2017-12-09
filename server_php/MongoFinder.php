c75168035b42ebb0778761da41817a20198b4b71
antoniodm <33807233+antoniodm@users.noreply.github.com>
2017-12-09 16:23:46 +0100

    Add files via upload

--- server_php/MongoFinder.php
+++ server_php/MongoFinder.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 		$arr = array();
    		foreach($result as $c){
 
-	       	$temp = array("id_sensor" => $c["id_sensore"], "type" => $c["pianta"], "code" => $c["id_arduino"]);
+   		    $temp = array("id_sensor" => $c["id_sensore"], "type" => $c["pianta"], "code" => $c["id_arduino"], "active" => $c["attivo"]);
 	       	 array_push($arr, $temp);
 
 		}
@@ -47,28 +47,106 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     	echo json_encode(array("result" => $arr));	
 
 	}
-
-	if ($nVar == 3) {
-
-		$id = $_GET['id'];
-
-		$data1 = $_GET['data1'];
-
-		$data2 = $_GET['data2'];
-
-		$rangeQuery = array('id_sensor' => $id, 'data' => array( '$gt' => $data1, '$lt' => $data2 ));
-
-		$result = $mcollection->find($rangeQuery); 
-
-		foreach($result as $c){
-
-	       	$temp = array("id_sensor" => $c["id_sensore"], "humidity" => $c["umidita"], "temperature" => $c["temperatura"], "datetime" => $c["data"]);
-	       	 array_push($arr, $temp);
-
-		}
-
-    	echo json_encode($arr);	
-
+	
+	if ($nVar == 2) {
+	    $id = (int)$_GET['id'];
+	    $name = $_GET['setName'];
+	    
+	    $query = ['id_sensore' => $id];
+	    $cambia = array('$set' => array('pianta' => $name));
+	    
+	    $result = $scollection->updateOne( $query, $cambia);
+	    
+	    echo "200";
+	    
+	    
 	}
+
+
+	if ($nVar == 3 && $_GET['archive'] != NULL) {
+
+	    $id_arduino = $_GET['code'];
+	    
+	    $id_sensore = (int)$_GET['id'];
+	    
+	    
+	    $sensore = $scollection->findOne(['id_sensore' => $id_sensore]);
+	    
+	    $nome_pianta = (string)$sensore['pianta'];
+	    
+	    $db_archiviate = $client->db_archiviate;
+	    
+	    $archiviate = $db_archiviate->$nome_pianta; //nuova collezione
+	    $filter = ['id_sensore' => $id_sensore];
+	    $options = array("sort" => array("_id" => 1)); //ordine crescente
+	    $cursor = $mcollection->find($filter, $options);
+	    foreach($cursor as $misurazione){ //cancella ogni misurazioni con determinato id_sensore e la inserisce nella nuova collezione
+	        $result = $archiviate->insertOne(['pianta' => $nome_pianta, 'data' => $misurazione['data'], 'umidita' => $misurazione['umidita'], 'temperatura' => $misurazione['temperatura'] ]);
+	        $mcollection->DeleteOne(['_id' => $misurazione['_id']]);
+	    }
+	    
+	    $query = ['id_sensore' => $id_sensore];
+	    $cambia = array('$set' => array('attivo' => false, 'pianta' => 'indefinito')); //aggiorna il sensore togliendo il nome della pianta e mettendo "attivo" a false
+	    
+	    $result = $scollection->updateOne( $query, $cambia);
+	    
+	    
+	    
+	    echo "200";
+	    
+	   /* $result = $mcollection->find( [ 'id_sensore' => (int)$id_sensore ] );
+	    $temp1 = array("id_sensore" => $id_sensore, "id_arduino" => $id_sensore );
+	    $arr = array();
+	    array_push($arr, $temp1);
+	    foreach($result as $c){
+	        $temp = array( "umidita" => $c["umidita"], "temperatura" => $c["temperatura"], "data" => $c["data"]);
+	        array_push($arr, $temp);
+	    }
+	    // creating object of SimpleXMLElement
+	    $xml = new SimpleXMLElement("<?xml version=\"1.0\"?><misurazione></misurazione>");
+	    // function call to convert array to xml
+	    array_to_xml($arr,$xml);
+	    
+	    //saving generated xml file
+	    echo $xml;*/
+        
+	}
+	 
+
+	
 }
+
+function array_to_xml($arr, &$xml) {
+    foreach($arr as $key => $value) {
+        if(is_array($value)) {
+            $key = is_numeric($key) ? "item$key" : $key;
+            $subnode = $xml->addChild("$key");
+            array_to_xml($value, $subnode);
+        }
+        else {
+            $key = is_numeric($key) ? "item$key" : $key;
+            $xml->addChild("$key","$value");
+        }
+    }
+}
+
+/*if ($nVar == 3) {
+
+$id = $_GET['id'];
+
+$data1 = $_GET['data1'];
+
+$data2 = $_GET['data2'];
+
+$rangeQuery = array('id_sensor' => $id, 'data' => array( '$gt' => $data1, '$lt' => $data2 ));
+
+$result = $mcollection->find($rangeQuery);
+
+foreach($result as $c){
+
+$temp = array("id_sensor" => $c["id_sensore"], "humidity" => $c["umidita"], "temperature" => $c["temperatura"], "datetime" => $c["data"]);
+array_push($arr, $temp);
+
+}*/
+
 ?>
--- server_php/MongoUpdater.php
+++ server_php/MongoUpdater.php
@@ -4,24 +4,26 @@ $client = new MongoDB\Client("mongodb://localhost:27017"); //si connette al db
 $mcollection = $client->db_misurazioni->misurazioni; 
 $scollection = $client->db_sensori->sensori; 
 
+
 //salvo i valori delle rispettive chiavi e li converto nei tipi corretti
 $id_sensore = (int)$_POST['id_sensore'];
 $id_arduino = (int)$_POST['id_arduino'];
 $umidita = (float)$_POST['umidita'];
 $temperatura = (float)$_POST['temperatura'];
 $data = $_POST['data'];
-//$test = $data;
-//$data = new MongoDB\BSON\UTCDateTime($data);
-//$data = new MongoDB\BSON\Timestamp(1, date($data));; //risolvere il problema noto della data indietro di una ora
-	
-	
 $cursore = $scollection->findOne([ 'id_sensore' => $id_sensore ]);
+$spenti = $scollection->findOne([ 'id_sensore' => $id_sensore, 'attivo' => false ]);
 //se il sensore non esiste inserisce un nuovo sensore nel db db_sensori
 //ma come gli assegno il nome della pianta?
 if (empty($cursore)){
 	$result = $scollection->insertOne( [ 'id_sensore' => $id_sensore, 'id_arduino' => $id_arduino, 'attivo' => true, 'pianta' => 'indefinito']);
 }
+if (!empty($spenti)){
+    $cambia = array('$set' => array('attivo' => true )); //aggiorna il sensore togliendo il nome della pianta e mettendo "attivo" a false
+    
+    $result = $scollection->updateOne( $cursore, $cambia);
+}
 $result = $mcollection->insertOne( [ 'id_sensore' => $id_sensore, 'umidita' => $umidita, 'temperatura' => $temperatura, 'data' => $data ] ); 
-s
+
  
 ?>
